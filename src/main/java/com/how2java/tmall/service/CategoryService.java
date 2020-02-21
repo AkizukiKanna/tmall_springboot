02b09ca3d044d96217915b584b5b6a30c90240cc
AkizukiKanna <sunfengkun@hotmail.com>
2020-02-21 23:07:54 +0800

    no msg

--- src/main/java/com/how2java/tmall/service/CategoryService.java
+++ src/main/java/com/how2java/tmall/service/CategoryService.java
@@ -2,7 +2,11 @@ package com.how2java.tmall.service;
 
 import com.how2java.tmall.dao.CategoryDAO;
 import com.how2java.tmall.pojo.Category;
+import com.how2java.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
@@ -14,9 +18,22 @@ public class CategoryService {
     @Autowired
     CategoryDAO categoryDAO;
 
-    public List<Category> list(){
-        //2.0版本需要用Sort.by(),原因是Sort类的构造方法被私有化
+    //navigatePages分页导航栏要显示出的页的数量
+    public Page4Navigator<Category> list(int start, int size, int navigatePages){
         Sort sort = Sort.by(Sort.Direction.DESC, "id");
+        //Pageable pageable = new PageRequest(start, size,sort);过时。
+        //PageRequest存放分页信息，第一个参数page(start)是页码，就是本次查询要查询哪一页，默认是从0开始的。
+        Pageable pageable = PageRequest.of(start, size, sort);
+        //??需要把查询结果分页，所以使用findAll(Pageable)
+        Page pageFromJPA = categoryDAO.findAll(pageable);
+
+        return new Page4Navigator<>(pageFromJPA,navigatePages);
+    }
+
+    public List<Category> list(){
+        //创建一个 Sort 对象，表示通过 id 倒排序。2.0版本需要用Sort.by(),原因是Sort类的构造方法被私有化
+        Sort sort = Sort.by(Sort.Direction.DESC, "id");
+        //??只需要列出所有的查询结果，不需要分页，所以使用findAll(Sort)
         return categoryDAO.findAll(sort);
     }
 }
--- src/main/java/com/how2java/tmall/web/CategoryController.java
+++ src/main/java/com/how2java/tmall/web/CategoryController.java
@@ -2,8 +2,10 @@ package com.how2java.tmall.web;
 
 import com.how2java.tmall.pojo.Category;
 import com.how2java.tmall.service.CategoryService;
+import com.how2java.tmall.util.Page4Navigator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,8 +23,10 @@ public class CategoryController {
      * 对于categories 访问，会获取所有的 Category对象集合，并返回这个集合。
      * 因为是声明为 @RestController， 所以这个集合，又会被自动转换为 JSON数组抛给浏览器。
      */
-    @GetMapping(value = "/categories")
-    public List<Category> list() throws Exception{
-        return  categoryService.list();
+    @GetMapping("/categories")
+    public Page4Navigator<Category> list(@RequestParam(value = "start", defaultValue = "0") int start, @RequestParam(value = "size", defaultValue = "5") int size) throws Exception {
+        start = start<0?0:start;
+        Page4Navigator<Category> page =categoryService.list(start, size, 5);  //5表示导航分页最多有5个，像 [1,2,3,4,5] 这样
+        return page;
     }
 }
